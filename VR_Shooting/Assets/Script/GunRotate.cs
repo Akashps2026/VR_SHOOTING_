e71edf6e0c5f87761f900a662cbf9589ed600a06
Akashps2026 <akashps078@gmail.com>
2025-09-16 10:43:04 +0530

    Add particals

---
+++ VR_Shooting/Assets/Script/GunRotate.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GunRotate : MonoBehaviour
+{
+
+    public float  Speed =10;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.Rotate(Vector3.up*Speed*Time.deltaTime);
+    }
+}
