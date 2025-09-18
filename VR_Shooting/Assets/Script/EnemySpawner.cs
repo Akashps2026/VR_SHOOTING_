f7765d7cda43083728c0c53889bd8f3182b3b936
Akashps2026 <akashps078@gmail.com>
2025-09-18 17:39:33 +0530

    Add Grass Asset

--- VR_Shooting/Assets/Script/EnemySpawner.cs
+++ VR_Shooting/Assets/Script/EnemySpawner.cs
@@ -6,13 +6,21 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private GameObject enemyPrefab;
     [SerializeField] private Transform spawnPoint;
     [SerializeField] private bool isGameOver = false;
+    public int enemyCount= 0;
+    public int enemyMaxCount = 15;
 
+
+    private void Start()
+    {
+        Debug.Log(this.gameObject.name);
+    }
 
     private IEnumerator EnemySpawnCoroutine()
     {
-        while (!isGameOver)
+        while (!isGameOver && enemyCount<enemyMaxCount)
         {
             GameObject enemyInstance = Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity, spawnPoint);
+            enemyCount++;   
             yield return new WaitForSeconds(0.5f);
         }
     }
