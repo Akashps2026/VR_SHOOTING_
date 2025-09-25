2fd71021bc6427b484522813eeffe4ebf513c5c3
Akashps2026 <akashps078@gmail.com>
2025-09-25 16:44:36 +0530

    Finish The Project

--- VR_Shooting/Assets/Script/Enemy.cs
+++ VR_Shooting/Assets/Script/Enemy.cs
@@ -10,22 +10,8 @@ public class Enemy : MonoBehaviour
     [SerializeField] private NavMeshAgent navMeshAgent;
     [SerializeField] private GameObject player;
     //[SerializeField]private EnemyKillManager killManager;
-    public GameObject winPanel;            // Assign in Inspector
-    public TextMeshProUGUI killText;       // Assign UI text in Inspector
-    public int killTarget = 15;            // Target kills to win
-    private int currentKills = 0;
-    [SerializeField] private GameObject enemyPrefab;
-    [SerializeField] private Transform spawnPoint;
-    [SerializeField] private bool isGameOver = false;
-    public int enemyCount = 0;
-    public int enemyMaxCount = 15;
     public int scoreValue = 10;
     public GameManager gameManager;
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-
-
     void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
@@ -37,36 +23,23 @@ public class Enemy : MonoBehaviour
         if (gameManager == null) gameManager = FindObjectOfType<GameManager>();
         player = GameObject.Find("Player");
         Debug.Log(this.gameObject.name);
-        UpdateKillUI();
-        if (winPanel != null) winPanel.SetActive(false); // hide at start
+        
+         // hide at start
         //killManager = FindObjectOfType<EnemyKillManager>();
 
     }
-
-
     private void Update()
     {
         navMeshAgent.destination = player.transform.position;
     }
-    private IEnumerator EnemySpawnCoroutine()
-    {
-        while (!isGameOver && enemyCount < enemyMaxCount)
-        {
-            GameObject enemyInstance = Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity, spawnPoint);
-            enemyCount++;
-            yield return new WaitForSeconds(0.5f);
-        }
-    }
-    public void SpawnEnemy()
-    {
-        StartCoroutine(EnemySpawnCoroutine());
-    }
+   
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Bullet"))
         {
-            gameManager?.AddScore(scoreValue);
+            gameManager.AddScore(scoreValue);
+            gameManager.AddKill();
             // play hit FX or animation here
             gameObject.SetActive(false);          // simple "destroy"
             Destroy(other.gameObject);            // remove bullet
@@ -80,30 +53,6 @@ public class Enemy : MonoBehaviour
             //killManager.AddKill();
         }
     }
-
-    public void AddKill()
-    {
-        currentKills++;
-        UpdateKillUI();
-
-        if (currentKills >= killTarget)
-        {
-            ShowWinPanel();
-        }
-    }
-
-    void UpdateKillUI()
-    {
-        if (killText != null)
-            killText.text = "Kills: " + currentKills + " / " + killTarget;
-    }
-
-    void ShowWinPanel()
-    {
-        winPanel.SetActive(true);
-        Time.timeScale = 0f; // pause game
-    }
-
     public void RestartGame()
     {
         Time.timeScale = 1f;
--- VR_Shooting/Assets/Script/GameManager.cs
+++ VR_Shooting/Assets/Script/GameManager.cs
@@ -1,21 +1,64 @@
 // GameManager.cs
+using System.Collections;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private bool isGameOver = false;
+    public int enemyCount = 0;
+    public int enemyMaxCount = 15;
     public int score;
     public Text scoreText;
-    [SerializeField] private Enemy enemySpawner;
-   
+    public int killTarget = 15;            
+    private int currentKills = 0;
+    public TextMeshProUGUI killText;
+    public GameObject winPanel;
     void Start()
     {
         UpdateUI();
-        enemySpawner.SpawnEnemy();
+        SpawnEnemy();
+        UpdateKillUI();
+        if (winPanel != null) winPanel.SetActive(false);
     }
+    private IEnumerator EnemySpawnCoroutine()
+    {
+        while (!isGameOver && enemyCount < enemyMaxCount)
+        {
+            GameObject enemyInstance = Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity, spawnPoint);
+            enemyCount++;
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+    public void SpawnEnemy()
+    {
+        StartCoroutine(EnemySpawnCoroutine());
+    }
+    public void AddKill()
+    {
+        currentKills++;
+        UpdateKillUI();
 
-   
+        if (currentKills >= killTarget)
+        {
+            ShowWinPanel();
+        }
+    }
+    void UpdateKillUI()
+    {
+        if (killText != null)
+            killText.text = "Kills: " + currentKills + " / " + killTarget;
+    }
+    void ShowWinPanel()
+    {
+        winPanel.SetActive(true);
+        Time.timeScale = 0f; // pause game
+    }
     public void AddScore(int amount)
     {
         score += amount;
